b78206d10598aa88034d3fda7c1e1ccc11079c55
Kris Winer <kriswiner@users.noreply.github.com>
2014-12-23 19:42:08 -0800

    Update LSM9DS1_MS5611_BasicAHRS_t3.ino

--- LSM9DS1_MS5611_BasicAHRS_t3.ino
+++ LSM9DS1_MS5611_BasicAHRS_t3.ino
@@ -450,14 +450,14 @@ void loop()
   
   // Sensors x, y, and z axes of the accelerometer and gyro are aligned. The magnetometer  
   // the magnetometer z-axis (+ up) is aligned with the z-axis (+ up) of accelerometer and gyro, but the magnetometer
-  // x-axis is aligned with the -y axis of the gyro and the magnetometer y axis is aligned with the -x axis of the gyro!
+  // x-axis is aligned with the -x axis of the gyro and the magnetometer y axis is aligned with the y axis of the gyro!
   // We have to make some allowance for this orientation mismatch in feeding the output to the quaternion filter.
   // For the LSM9DS1, we have chosen a magnetic rotation that keeps the sensor forward along the x-axis just like
   // in the LSM9DS0 sensor. This rotation can be modified to allow any convenient orientation convention.
   // This is ok by aircraft orientation standards!  
   // Pass gyro rate as rad/s
-  MadgwickQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f,  -my,  -mx, mz);
-//  MahonyQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f, -my, -mx, mz);
+  MadgwickQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f,  -mx,  my, mz);
+//  MahonyQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f, -mx, my, mz);
 
     // Serial print and/or display at 0.5 s rate independent of data rates
     delt_t = millis() - count;
